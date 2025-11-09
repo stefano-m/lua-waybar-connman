3b7f2cef5efebabc39baadace710ad9a5d50710f
Stefano Mazzucco <stefano@curso.re>
2025-11-09 18:09:25 +0000

    Set up waybar module for connman

---
+++ flake.nix
@@ -0,0 +1,107 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+
+    lua-connman_dbus.url = "github:stefano-m/lua-connman_dbus/master";
+    lua-connman_dbus.inputs.nixpkgs.follows = "nixpkgs";
+
+  };
+
+  outputs = { self, nixpkgs, lua-connman_dbus }:
+    let
+
+      flakePkgs = import nixpkgs {
+        system = "x86_64-linux";
+        overlays = [ self.overlays.default ];
+      };
+
+      currentVersion = "0.1";
+
+      buildPackage = pname: luaPackages: with luaPackages;
+        buildLuaPackage rec {
+          name = "${pname}-${version}";
+          inherit pname;
+          version = "${currentVersion}-${self.shortRev or "dev"}";
+
+          src = ./.;
+
+          propagatedBuildInputs = [ lua lgi cjson connman_dbus flakePkgs.glib ];
+
+          buildInputs = [ luacheck ];
+
+          nativeBuildInputs = [
+            flakePkgs.makeWrapper
+          ];
+
+          buildPhase = ":";
+
+          # FIXME: LUA_PATH brings in the test dependencies too that I don't
+          # really need and make the closure bigger!
+          installPhase = with flakePkgs; ''
+            mkdir -p "$out/share/lua/${lua.luaversion}"
+            cp -r src/${pname}.lua $out/share/lua/${lua.luaversion}/
+            chmod +x $out/share/lua/${lua.luaversion}/${pname}.lua
+
+            mkdir -p $out/bin
+            makeWrapper $out/share/lua/${lua.luaversion}/${pname}.lua $out/bin/${pname} \
+                --set-default LUA_PATH ";;" \
+                --suffix LUA_PATH ';' "$LUA_PATH" \
+                --set-default LUA_CPATH ";;" \
+                --suffix LUA_CPATH ';' "$LUA_CPATH" \
+                --set-default GI_TYPELIB_PATH : \
+                --suffix GI_TYPELIB_PATH : ${lib.getLib glib}/lib/girepository-1.0
+          '';
+
+          doCheck = true;
+          checkPhase = "luacheck src";
+
+        };
+
+    in
+    {
+      packages.x86_64-linux = rec {
+        default = lua_waybar_connman;
+        lua_waybar_connman = buildPackage "waybar_connman" flakePkgs.luaPackages;
+        lua52_waybar_connman = buildPackage "waybar_connman" flakePkgs.lua52Packages;
+        lua53_waybar_connman = buildPackage "waybar_connman" flakePkgs.lua53Packages;
+        luajit_waybar_connman = buildPackage "waybar_connman" flakePkgs.luajitPackages;
+      };
+
+      overlays.default = final: prev:
+        let
+          thisOverlay = final: prev: with self.packages.x86_64-linux; {
+            # NOTE: lua = prev.lua.override { packageOverrides = this: other: {... }}
+            # Seems to be broken as it does not allow to combine different overlays.
+
+            luaPackages = prev.luaPackages // {
+              waybar_connman = lua_waybar_connman;
+            };
+
+            lua52Packages = prev.lua52Packages // {
+              waybar_connman = lua52_waybar_connman;
+            };
+
+            lua53Packages = prev.lua53Packages // {
+              waybar_connman = lua53_waybar_connman;
+            };
+
+            luajitPackages = prev.luajitPackages // {
+              waybar_connman = luajit_waybar_connman;
+            };
+
+          };
+        in
+        # expose the other lua overlays together with this one.
+        (nixpkgs.lib.composeManyExtensions [ thisOverlay lua-connman_dbus.overlays.default ]) final prev;
+
+
+      devShells.x86_64-linux.default = flakePkgs.mkShell {
+        LUA_PATH = "./src/?.lua;./src/?/init.lua";
+
+        buildInputs = (with self.packages.x86_64-linux.lua53_waybar_connman; buildInputs ++ propagatedBuildInputs) ++ (with flakePkgs; [
+          nixpkgs-fmt
+          luarocks
+        ]);
+      };
+    };
+}
---
+++ src/waybar_connman.lua
@@ -0,0 +1,102 @@
+#!/usr/bin/env lua
+local GLib = require("lgi").GLib
+local json = require("cjson.safe")
+local string = string
+local arg = arg
+
+local SIGINT = 2
+local SIGTERM = 15
+
+local function format(service)
+
+  local css_class = "disconnected"
+
+  if service.State == "ready" or service.State == "online" then
+    css_class = "connected"
+  end
+
+  return {
+    text = service.State,
+    alt =  service.Type,
+    tooltip =  string.format("%s (%s - %s)%s",
+                             service.Name or "<hidden>",
+                             service.IPv4.Address or "<none>",
+                             service.Ethernet.Interface,
+                             (service.Error and " " .. service.Error or "")),
+    class = css_class,
+    percentage =  service.Strength or "",
+  }
+end
+
+local relevant_property_names = {
+  State = true,
+  Type = true,
+  Name = true,
+  IPv4 = true,
+  Ethernet = true,
+  Error = true,
+}
+
+local function update(service)
+  service:connect_signal(
+    function(svc, prop_name)
+      if relevant_property_names[prop_name] then
+        local output = format(svc)
+        print(json.encode(output))
+      end
+    end,
+    "PropertyChanged"
+  )
+end
+
+local function run()
+
+  local connman = require("connman_dbus")
+
+  local current = connman.services[1]
+
+  local output = format(current)
+  print(json.encode(output))
+
+  update(current)
+
+  connman:connect_signal(
+    function (mgr)
+      current = mgr.services[1]
+      if current then
+        update(current, output)
+      else
+        print(json.encode({
+                  text = mgr.State,
+                  alt = "offline",
+                  tooltip = "disabled",
+                  class = "disconnected",
+                  percentage = "",
+        }))
+      end
+    end,
+    "ServicesChanged"
+  )
+
+  local main_loop = GLib.MainLoop()
+
+  local function exit_on_signal()
+    print(json.encode({
+              text = "exited",
+              alt = "quit",
+              tooltip = "exited",
+              class = "quit",
+              percentage = "",
+    }))
+    main_loop:quit()
+  end
+
+  GLib.unix_signal_add(GLib.PRIORITY_HIGH, SIGINT, exit_on_signal)
+  GLib.unix_signal_add(GLib.PRIORITY_HIGH, SIGTERM, exit_on_signal)
+
+  main_loop:run()
+end
+
+if arg and arg[1] == "run" then
+  run()
+end
